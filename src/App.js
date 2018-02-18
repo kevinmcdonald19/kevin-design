c9114cde296d1d46b195aea8e05ae265b05dbddb
Kev <kev@Kevin-Personal-Mac.local>
2018-02-17 19:13:56 -0600

    change last name

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Kevin McDonald Design</h1>
+          <h1 className="App-title">Kevin Trepanier Design</h1>
         </header>
         
         <Details kevin={"kevin"}/>
